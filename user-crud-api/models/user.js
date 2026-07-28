a6a872d788ebee9a9cde316af385465b5616dad8
manisaimuralithatipelli <manisaimuralithatipelli1@gmail.com>
2026-07-28 20:08:16 +0530

    Update user.js

--- user-crud-api/models/user.js
+++ user-crud-api/models/user.js
@@ -1,1 +1,26 @@
- 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
