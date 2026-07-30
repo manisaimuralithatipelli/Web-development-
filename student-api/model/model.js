a2a8f48b9605e5ee31764cc5448d2d401e46f9e7
manisaimuralithatipelli <manisaimuralithatipelli1@gmail.com>
2026-07-30 13:36:13 +0530

    Update model.js

--- student-api/model/model.js
+++ student-api/model/model.js
@@ -1,1 +1,31 @@
+const mongoose=require("mongoose");
+
+const studentSchema=new mongoose.Schema({
+     first_name:{
+          type:String,
+          required:true,
+        },
+     last_name:{
+          type:String,
+          required:true,
+        },
+    email:{
+         type:String,
+         required:true,
+         unique:true,
+    },
+   gender:{
+        type:String,
+        required:true,
+   },
+   age:{
+       type:Number,
+       required:true,
+       min:1,
+   },
+});
+
+const Student=mongoose.model("Student",studentSchema);
+
+module.exports=Student;
 
